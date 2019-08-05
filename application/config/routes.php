c7060525bc1886be26d03b89f1c6ae6e41f7a1a5
Bekor90 <31639240+Bekor90@users.noreply.github.com>
2019-08-05 17:37:16 -0500

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,27 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $route['default_controller'] = 'Home';
-$route['Ingresar'] = 'login/Login/ValidarIngreso';
-$route['Dashboard'] = 'dashboard/Dashboard_controller'; 
-$route['Dashboard/usuarios'] = 'dashboard/Dashboard_controller/usuarios';
-$route['Dashboard/tareas'] = 'dashboard/Dashboard_controller/tareas';
-$route['Dashboard/categorias'] = 'dashboard/Dashboard_controller/categorias';
-$route['Dashboard/salir'] = 'dashboard/Dashboard_controller/salir';
+$route['Ingresar'] = 'Login_controller/ValidarIngreso';
+$route['Dashboard'] = 'Dashboard_controller'; 
+$route['Dashboard/usuarios'] = 'Dashboard_controller/usuarios';
+$route['Dashboard/tareas'] = 'Dashboard_controller/tareas';
+$route['Dashboard/categorias'] = 'Dashboard_controller/categorias';
+$route['Dashboard/salir'] = 'Dashboard_controller/salir';
 
-$route['registrar/usuario'] = 'usuarios/Usuarios_controller/registrarUsuarios';
-$route['editar/usuario/(:num)'] = 'usuarios/Usuarios_controller/editarUsuarios/$1';
-$route['actualizar/usuario'] = 'usuarios/Usuarios_controller/editar';
+$route['registrar/usuario'] = 'Usuarios_controller/registrarUsuarios';
+$route['editar/usuario/(:num)'] = 'Usuarios_controller/editarUsuarios/$1';
+$route['actualizar/usuario'] = 'Usuarios_controller/editar';
 $route['eliminar/usuario/(:num)'] = 'usuarios/Usuarios_controller/eliminarUsuario/$1';
 
-$route['registrar/tarea'] = 'tareas/Tareas_controller/registrarTareas';
-$route['editar/tarea/(:num)'] = 'tareas/Tareas_controller/editarTarea/$1';
-$route['actualizar/tarea'] = 'tareas/Tareas_controller/actualizar';
+$route['registrar/tarea'] = 'Tareas_controller/registrarTareas';
+$route['editar/tarea/(:num)'] = 'Tareas_controller/editarTarea/$1';
+$route['actualizar/tarea'] = 'Tareas_controller/actualizar';
 $route['eliminar/tarea/(:num)'] = 'tareas/Tareas_controller/eliminarTarea/$1';
 
-$route['registrar/categoria'] = 'categoria/Categoria_controller/registrarCategoria';
-$route['editar/categoria/(:num)'] = 'categoria/Categoria_controller/editarCategoria/$1';
-$route['actualizar/categoria'] = 'categoria/Categoria_controller/actualizar';
-$route['eliminar/categoria/(:num)'] = 'categoria/Categoria_controller/eliminarCategoria/$1';
+$route['registrar/categoria'] = 'Categoria_controller/registrarCategoria';
+$route['editar/categoria/(:num)'] = 'Categoria_controller/editarCategoria/$1';
+$route['actualizar/categoria'] = 'Categoria_controller/actualizar';
+$route['eliminar/categoria/(:num)'] = 'Categoria_controller/eliminarCategoria/$1';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
