e0d42abed7cfd21a802fcae01ff32e58ea04320b
Bekor90 <31639240+Bekor90@users.noreply.github.com>
2019-08-05 20:22:41 -0500

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$data['result'] = false;
+		$data['error'] = false;
 		$data['mensaje'] = '';
 		$this->load->view('login', $data);
 	}
